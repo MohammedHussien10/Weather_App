36dad07f9a1493641784ceb8f140726c2320cb55
MohammedHussien10 <mohammedhussien10101010@gmail.com>
2025-03-24 16:17:49 +0200

    add Repository

--- app/src/main/java/com/example/weatherapp/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/WeatherRepository.kt
@@ -4,7 +4,13 @@ import com.example.weatherapp.data.remote.WeatherResponse
 import com.example.weatherapp.data.remote.WeatherApiService
 
 class WeatherRepository(private val apiService: WeatherApiService) {
+    // دالة جلب الطقس باستخدام اسم المدينة
     suspend fun getWeather(city: String, apiKey: String): WeatherResponse {
         return apiService.getWeather(city, apiKey)
+    }
+
+    // دالة جلب الطقس باستخدام الإحداثيات الجغرافية (الخط العرض والطول)
+    suspend fun getWeatherByLocation(latitude: Double, longitude: Double, apiKey: String): WeatherResponse {
+        return apiService.getWeatherByLocation(latitude, longitude, apiKey)
     }
 }
