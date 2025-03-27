822d4b2f4133434ac4738f412b24498279cf9a24
MohammedHussien10 <mohammedhussien10101010@gmail.com>
2025-03-27 22:35:02 +0200

    Update design for Time and Date in HomeScreen

--- app/src/main/java/com/example/weatherapp/data/remote/Units.kt
+++ app/src/main/java/com/example/weatherapp/data/remote/Units.kt
@@ -1,12 +1,32 @@
 package com.example.weatherapp.data.remote
 
 
+import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.*
 
+//Date and Time
 fun convertTimestampToDate(timestamp: Long): String {
     val date = Date(timestamp * 1000)
     val format = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
     return format.format(date)
 
+}
+
+//DateOnly
+fun convertTimestampToDateOnly(timestamp: Long): String {
+    val date = Date(timestamp * 1000)
+    val format = SimpleDateFormat("EEE, dd MMM", Locale.getDefault()) // EEE = Fri, MMM = Feb
+    return format.format(date)
+}
+
+
+//TimeOnly
+fun convertTimestampToTimeOnly(timestamp: Long): String {
+    val calendar = Calendar.getInstance().apply { timeInMillis = timestamp * 1000 }
+    val hour = calendar.get(Calendar.HOUR)
+    val minute = calendar.get(Calendar.MINUTE)
+    val amPm = if (calendar.get(Calendar.AM_PM) == Calendar.AM) "AM" else "PM"
+
+    return String.format("%02d:%02d %s", if (hour == 0) 12 else hour, minute, amPm)
 }
